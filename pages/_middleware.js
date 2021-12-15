1ce4d61b44ee718cd1e3f2f767d8dc92f353e4d4
r34mod <48959634+r34mod@users.noreply.github.com>
2021-12-15 09:13:23 +0100

    Add files via upload

---
+++ pages/_middleware.js
@@ -0,0 +1,22 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+
+export async function middleware(req){
+    const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+    const { pathname } = req.nextUrl;
+
+    /**
+     * It is a request for next-auth session and provider fetching
+     * the token exists
+     * 
+     */
+    if(pathname.includes('/api/auth') || token){
+        return NextResponse.next();
+    }
+
+    if(!token && pathname !== '/login'){
+        return NextResponse.redirect("/login");
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Player from '../components/Player';
+import { getSession } from "next-auth/react";
+import Center from '../components/Center';
+import Sidebar from '../components/Sidebar';
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="flex">
+        {/**Sidebar */}
+        <Sidebar/>
+        <Center />
+      </main>
+
+      <div className="sticky bottom-0">
+        <Player />
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return{
+    props:{
+      session,
+    },
+  };
+}
